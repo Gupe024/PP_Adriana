c736dee49e33a14530802dfbfdc848bf57588e1e
Canul <canuladriana24@gmail.com>
2024-06-10 23:41:01 -0600

    última modificación

--- app/src/main/java/com/example/eplanetas_a/Informacion.java
+++ app/src/main/java/com/example/eplanetas_a/Informacion.java
@@ -25,23 +25,30 @@ public class Informacion extends AppCompatActivity {
                 String[] titulos = getResources().getStringArray(R.array.Titulo_planeta);
                 String[] info1 = getResources().getStringArray(R.array.Info_1);
                 String[] info2 = getResources().getStringArray(R.array.info_2);
+                String[] info3 = getResources().getStringArray(R.array.info_3);
                 TypedArray imagen1 = getResources().obtainTypedArray(R.array.imagene_1);
                 TypedArray imagen2 = getResources().obtainTypedArray(R.array.imagen2);
+                TypedArray imagen3 = getResources().obtainTypedArray(R.array.imagen3);
 
                 TextView tituloInfo = findViewById(R.id.Tituloinfo);
                 TextView textoIzquierdo = findViewById(R.id.texto_izquierdo);
                 ImageView imagen1View = findViewById(R.id.imagen1);
                 ImageView imagen2View = findViewById(R.id.imagen2);
                 TextView textoCentrado = findViewById(R.id.texto_centrado);
+                TextView textoIzquierdo2 = findViewById(R.id.texto_izquierdo2);
+                ImageView imagen3View = findViewById(R.id.imagen3);
 
                 tituloInfo.setText(titulos[position]);
                 textoIzquierdo.setText(info1[position]);
                 imagen1View.setImageResource(imagen1.getResourceId(position, -1));
                 imagen2View.setImageResource(imagen2.getResourceId(position, -1));
                 textoCentrado.setText(info2[position]);
+                textoIzquierdo2.setText(info3[position]);
+                imagen3View.setImageResource(imagen3.getResourceId(position, -1));
 
                 imagen1.recycle();
                 imagen2.recycle();
+                imagen3.recycle();
             }
         }
 
@@ -49,7 +56,6 @@ public class Informacion extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Abrir la actividad de login
                 Intent intent = new Intent(Informacion.this, MainActivity.class);
                 startActivity(intent);
             }
@@ -59,10 +65,10 @@ public class Informacion extends AppCompatActivity {
         btnRegresar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Regresar a la ventana anterior
-                finish(); // Cierra esta actividad y regresa a la anterior
+                finish();
             }
         });
     }
 }
+
 
